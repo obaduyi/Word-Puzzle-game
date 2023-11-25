2ce35a7da0bad3e3b1eabb9c2b8463283402e58a
obaduyi <143231633+obaduyi@users.noreply.github.com>
2023-11-25 13:56:21 -0500

    Add files via upload

---
+++ python project/customizze.py
@@ -0,0 +1,21 @@
+# colors
+white = (200, 200, 200)
+black = (0, 0, 0)
+darkgray = (40, 40, 40)
+lightgrey = (100, 100, 100)
+green = (0, 200, 0)
+blue = (0, 0, 150)
+red = (150, 0, 0)
+yellow = (150, 150, 0)
+BGCOLOUR = darkgray
+
+#customization
+WIDTH = 600
+HEIGHT = 630
+FPS = 60
+title = "wordle game project"
+
+TILESIZE = 80
+GAPSIZE = 10
+MARGIN_X = int((WIDTH -(5* (TILESIZE + GAPSIZE)))/2)
+MARGIN_Y = int((HEIGHT -(6* (TILESIZE + GAPSIZE)))/2)
